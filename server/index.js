ee7feda6d1060c48e45163ef106b4441975e9f78
Brandon Rowe <brandonhrowe@gmail.com>
2019-10-04 11:25:00 -0400

    express middleware expanded, public files initiated, bulma added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 // Logging
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from /public directory
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// Send HTML file
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  let status = err.status || 500;
+  let message =
+    err.message || "Sorry, there's been a slight problem on our server";
+  res.status(status).send(message);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
