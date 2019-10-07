c60ec5bdf0e699e3aa64771317184ede1eccae51
Brandon Rowe <brandonhrowe@gmail.com>
2019-10-07 15:30:11 -0400

    portfolio route created

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -68,4 +68,70 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+const portfolioHelper = async portfolio => {
+  for (let key in portfolio) {
+    if (portfolio.hasOwnProperty(key)) {
+      // Query daily values for each symbol
+      const alphaData = await fetch(
+        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${key}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`
+      );
+      const newData = await alphaData.json();
+      // Retrieve the latest values
+      const lastRefreshed = newData["Meta Data"]["3. Last Refreshed"].split(
+        " "
+      )[0];
+      // Define variables for the stock values at the opening and close (latest) of the day
+      let closeVal = newData["Time Series (Daily)"][lastRefreshed]["4. close"];
+      let openVal = newData["Time Series (Daily)"][lastRefreshed]["1. open"];
+      // Store the close value to the object to be sent back
+      portfolio[key].currentValue = closeVal;
+      // Also store whether the change was positive/negative/neutral based on the opening and close values
+      if (closeVal > openVal) {
+        portfolio[key].change = "positive";
+      } else if (closeVal < openVal) {
+        portfolio[key].change = "negative";
+      } else {
+        portfolio[key].change = "neutral";
+      }
+    }
+  }
+  return portfolio;
+};
+
+router.get("/portfolio", async (req, res, next) => {
+  try {
+    if (!req.user) {
+      res.status(403).send("You must be logged in to access this route");
+    } else {
+      const { id: userId } = req.user;
+      // Get all of user's transactions
+      const transactions = await Transaction.findAll({
+        where: {
+          userId
+        }
+      });
+      // Establish a portfolio object to concatenate transactions with the same symbol, and add together the quantity for each
+      let portfolio = {};
+      for (let key in transactions) {
+        if (transactions.hasOwnProperty(key)) {
+          let symb = transactions[key].symbol;
+          if (!portfolio[symb]) {
+            portfolio[symb] = transactions[key];
+          } else {
+            portfolio[symb] = {
+              quantity: transactions[key].quantity + portfolio[symb].quantity
+            };
+          }
+        }
+      }
+      // Call helper function to get most up-to-date data
+      await portfolioHelper(portfolio);
+      // Final object sent out should have keys for each symbol and values for each with total quantity, latest value, and the polarity of change.
+      res.json(portfolio);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
