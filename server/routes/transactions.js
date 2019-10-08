ce67b309ef2a169fbb214490527df8cda3e6b9d9
Brandon Rowe <brandonhrowe@gmail.com>
2019-10-07 21:04:20 -0400

    portfolio route updated

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -8,6 +8,7 @@ router.get("/", async (req, res, next) => {
       res.status(403).send("You must be logged in to access this route");
     } else {
       const { id: userId } = req.user;
+      // Find all transactions for the current user
       const transactions = await Transaction.findAll({
         where: {
           userId
@@ -25,11 +26,14 @@ router.post("/", async (req, res, next) => {
     if (!req.user) {
       res.status(403).send("You must be logged in to access this route");
     } else {
+      // Pull symbol and quantity from request body
       const { symbol, quantity } = req.body;
+      // Call AlphaVantage with the symbol to check latest value of stock
       const alphaData = await fetch(
         `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=60min&apikey=${process.env.ALPHAVANTAGE_API_KEY}`
       );
       const newData = await alphaData.json();
+      // If symbol is not found from AlphaVantage, throw 403 error
       if (newData["Error Message"]) {
         res
           .status(403)
@@ -37,30 +41,36 @@ router.post("/", async (req, res, next) => {
             "Sorry, that does not seem to be a valid symbol. Please try again."
           );
       } else {
+        // Store the time of the call, which is the key for finding the applicable info
         const lastRefreshed = newData["Meta Data"]["3. Last Refreshed"];
+        // Store the price of the stock upon buy, multiplying by 100 to store it in the database consistently with the balances
         const originalPrice = Math.floor(
           newData["Time Series (60min)"][lastRefreshed]["4. close"] * 100
         );
+        // If user does not have enough in their balance, throw 403 error
         if (req.user.balance < (originalPrice / 100) * req.body.quantity) {
           res
             .status(403)
             .send("Sorry, you do not have enough money to buy this stock");
+        } else {
+          const { id } = req.user;
+          const user = await User.findByPk(id);
+          // Otherwise, add transaction to database and associate it with user
+          const transaction = await Transaction.create({
+            symbol,
+            quantity,
+            originalPrice
+          });
+          await transaction.setUser(user);
+          // Update user's balance by subtracting transaction cost
+          const newBalance = Math.floor(
+            user.balance - transaction.originalPrice * transaction.quantity
+          );
+          await user.update({
+            balance: newBalance
+          });
+          res.status(201).json(transaction);
         }
-        const { id } = req.user;
-        const user = await User.findByPk(id);
-        const transaction = await Transaction.create({
-          symbol,
-          quantity,
-          originalPrice
-        });
-        await transaction.setUser(user);
-        const newBalance = Math.floor(
-          user.balance - transaction.originalPrice * transaction.quantity
-        );
-        await user.update({
-          balance: newBalance
-        });
-        res.status(201).json(transaction);
       }
     }
   } catch (error) {
@@ -69,33 +79,30 @@ router.post("/", async (req, res, next) => {
 });
 
 const portfolioHelper = async portfolio => {
-  for (let key in portfolio) {
-    if (portfolio.hasOwnProperty(key)) {
-      // Query daily values for each symbol
-      const alphaData = await fetch(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${key}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`
-      );
-      const newData = await alphaData.json();
-      // Retrieve the latest values
-      const lastRefreshed = newData["Meta Data"]["3. Last Refreshed"].split(
-        " "
-      )[0];
-      // Define variables for the stock values at the opening and close (latest) of the day
-      let closeVal = newData["Time Series (Daily)"][lastRefreshed]["4. close"];
-      let openVal = newData["Time Series (Daily)"][lastRefreshed]["1. open"];
-      // Store the close value to the object to be sent back
-      portfolio[key].currentValue = closeVal;
-      // Also store whether the change was positive/negative/neutral based on the opening and close values
-      if (closeVal > openVal) {
-        portfolio[key].change = "positive";
-      } else if (closeVal < openVal) {
-        portfolio[key].change = "negative";
-      } else {
-        portfolio[key].change = "neutral";
-      }
+  for (let i = 0; i < portfolio.length; i++) {
+    // Query daily values for each symbol
+    const alphaData = await fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${portfolio[i].symbol}&apikey=${process.env.ALPHAVANTAGE_API_KEY}`
+    );
+    const newData = await alphaData.json();
+    // Retrieve the latest values
+    const lastRefreshed = newData["Meta Data"]["3. Last Refreshed"].split(
+      " "
+    )[0];
+    // Define variables for the stock values at the opening and close (latest) of the day
+    let closeVal = newData["Time Series (Daily)"][lastRefreshed]["4. close"];
+    let openVal = newData["Time Series (Daily)"][lastRefreshed]["1. open"];
+    // Store the close value to the object to be sent back
+    portfolio[i].currentValue = closeVal;
+    // Also store whether the change was positive/negative/neutral based on the opening and close values
+    if (closeVal > openVal) {
+      portfolio[i].change = "positive";
+    } else if (closeVal < openVal) {
+      portfolio[i].change = "negative";
+    } else {
+      portfolio[i].change = "neutral";
     }
   }
-  return portfolio;
 };
 
 router.get("/portfolio", async (req, res, next) => {
@@ -111,22 +118,28 @@ router.get("/portfolio", async (req, res, next) => {
         }
       });
       // Establish a portfolio object to concatenate transactions with the same symbol, and add together the quantity for each
-      let portfolio = {};
-      for (let key in transactions) {
-        if (transactions.hasOwnProperty(key)) {
-          let symb = transactions[key].symbol;
-          if (!portfolio[symb]) {
-            portfolio[symb] = transactions[key];
-          } else {
-            portfolio[symb] = {
-              quantity: transactions[key].quantity + portfolio[symb].quantity
-            };
-          }
+      let portfolio = [];
+      let hashMap = {};
+      let prevIdx = 0;
+      let newIdx = 0;
+      while (prevIdx < transactions.length) {
+        let symb = transactions[prevIdx].symbol;
+        if (!hashMap[symb] && hashMap[symb] !== 0) {
+          hashMap[symb] = newIdx;
+          portfolio[newIdx] = {
+            symbol: symb,
+            quantity: transactions[prevIdx].quantity
+          };
+          newIdx++;
+          prevIdx++;
+        } else {
+          portfolio[hashMap[symb]].quantity += transactions[prevIdx].quantity;
+          prevIdx++;
         }
       }
       // Call helper function to get most up-to-date data
       await portfolioHelper(portfolio);
-      // Final object sent out should have keys for each symbol and values for each with total quantity, latest value, and the polarity of change.
+      // Final array sent out should have symbols and values plus total quantity, latest value, and the polarity of change.
       res.json(portfolio);
     }
   } catch (error) {
